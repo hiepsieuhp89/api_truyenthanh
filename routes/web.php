b0085a1a9c62240e5aacfc4d6700eb0991c57a18
xuantung <tungcan2000@gmail.com>
2022-04-01 20:29:44 +0700

    bt

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->intended('https://dev.truyenthanh.org.vn/');
-});
+// Route::get('/', function () {
+//     return redirect()->intended('https://dev.truyenthanh.org.vn/');
+// });
