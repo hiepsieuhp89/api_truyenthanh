254fa0a13c37218ac080b99aefd619726efe222f
xuantung <tungcan2000@gmail.com>
2022-04-01 21:39:52 +0700

    bt

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\AreaController;
 use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\API\ProgramController as ProgramAPIController;
+use App\Http\Controllers\API\DeviceController as DeviceAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,11 +26,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('devices/area/{name}', [DeviceController::class, 'showByArea']);
 Route::get('devices/area/{name}/schedule/{date}', [DeviceController::class, 'showByDate']);
-Route::apiResource('devices', DeviceController::class);
 
 Route::get('devices/area/', [DeviceController::class, 'showByArea']);
-
-
 
 Route::post('devices/area/linh-dam/play', [DeviceController::class, 'playNow']);
 
@@ -37,6 +36,12 @@ Route::get('play/{deviceCode}/{stringencode}', [ProgramController::class, 'play'
 Route::get('area', [AreaController::class, 'index']);
 
 Route::get('area/{id}/devices', [AreaController::class, 'getDevices']);
+
+//API Resource
+Route::apiResource('resource/devices', DeviceAPIController::class);
+Route::apiResource('resource/programs', ProgramAPIController::class);
+
+
 
 
 
